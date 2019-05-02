cc486281abf76740993373900e557e38b944cd55
dayane.mastelari@aelian.com.br <>
2019-05-02 17:52:07 -0300

    ator edit

--- Controller/AtorsController.php
+++ Controller/AtorsController.php
@@ -27,14 +27,13 @@ class AtorsController extends AppController {
     public function edit($id = null) {
         if (!empty($this->request->data)) {
             if ($this->Ator->saveAll($this->request->data)) {
-                $this->Flash->set('Ator cadastrado com sucesso');
+                $this->Flash->set('Ator alterado com sucesso');
                 $this->redirect('/ators');
             }
         } else {            
             $fields = array('Ator.id', 'Ator.nome', 'Ator.nascimento');
             $conditions = array('Ator.id' => $id);
             $this->request->data = $this->Ator->find('first', compact('fields', 'conditions'));
-
         }
         $fields = array('Filme.id', 'Filme.nome');
         $order = array('Filme.nome' => 'asc');
